1e930f36f808051f18f6080edc7fe18aaef63b2e
cjkeilly <christopher.keilly19@stjohns.edu>
2023-03-16 13:19:13 -0400

    before attempting onsubmit

---
+++ task.js
@@ -0,0 +1,12 @@
+// start with an empty array
+var tasks = [];
+
+// open the form when button is clicked
+function openForm() {
+    document.getElementById("add-form").style.display = "block";
+}
+
+// close the form when the add task button is clicked
+function closeForm() {
+    document.getElementById("add-form").style.display = "none";
+}
